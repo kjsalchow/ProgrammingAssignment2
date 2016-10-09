3f257b566577db2c10c8884c2c4d4e8e4f978873
KJ Salchow <kjsalchow@me.com>
2016-10-09 15:37:02 -0500

    Added completed cachematrix.R file Added cacheMatrixTestData.R file for use in testing#

---
+++ cacheMatrixTestDate.R
@@ -0,0 +1,3 @@
+## Instantiate two invertible test matrices
+a <- matrix(data = c(1,0,5,2,1,6,3,4,0),nrow = 3, ncol = 3)
+b <- matrix(data = c(1,1,1,3,4,3,3,3,4),nrow = 3, ncol = 3)
--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## instantiates an object of type makeCacheMatrix consisting of matrix and,
+## the inverse of the matrix in cache.
 makeCacheMatrix <- function(x = matrix()) {
-
+    cached <- NULL
+    set <- function(makeCache) {
+        x <<- makeCache
+        cached <<- NULL
+    }
+    get <- function() x
+    setCache <- function(solved) cached <<- solved
+    getCache <- function() cached
+    list(set=set, get=get, setCache=setCache, getCache=getCache)
 }
 
 
-## Write a short comment describing this function
+## Returns the inversion of the supplied matrix either from cache, or by,
+## calculating it, putting it in cache and then returning it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    cached <- x$getCache()
+    if(!is.null(cached)){
+        message("getting cached data")
+        return(cached)
+    }
+    data <-x$get()
+    cached <- solve(data, ...)
+    x$setCache(cached)
+    cached
 }
